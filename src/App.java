0abdd0b9d8f7757669479860cbe21f584d58d9e6
Melchiah <melchiahopleda1999@gmail.com>
2022-12-21 08:46:04 +0800

    MILESTONE A: Show the GUI

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class App {
+public class App extends JFrame{
     private JPanel pnlMain;
     private JRadioButton rbCustomer;
     private JRadioButton rbClerk;
@@ -30,6 +30,13 @@ public class App {
 
     public static void main(String[] args) {
         // add here how to make GUI visible
+        App app = new App();
+        app.setTitle("Leap Year Checker");
+        app.setContentPane(app.pnlMain);
+        app.setSize(700, 500);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+        app.setLocationRelativeTo(null);
     }
 
     static void giveReward(int n) {
